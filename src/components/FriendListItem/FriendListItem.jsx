0d54df4d4be7de00af853d09e90659195af6f9df
Gaudi <goncharov.kostiantyn@gmail.com>
2024-05-23 02:55:06 +0300

    fix second task

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
-import { FriendsItem, FriendsStatus, FriendsAvatar, FriendsName } from "./FriendListItem.styled"
+import { FriendsItem, FriendsStatus, FriendsAvatar, FriendsName, FriendStatusText } from "./FriendListItem.styled"
 
 const FriendListItem = ({avatar, name, isOnline}) => {
     return <FriendsItem>
                 <FriendsStatus isOnline={isOnline}></FriendsStatus>
                 <FriendsAvatar src={avatar} alt={name} width="48"/>
                 <FriendsName>{name}</FriendsName>
+                <FriendStatusText isOnline={isOnline}>{isOnline ? '(Online)' : '(Offline)'}</FriendStatusText>
+        
             </FriendsItem>
 }
 
--- src/components/FriendListItem/FriendListItem.styled.jsx
+++ src/components/FriendListItem/FriendListItem.styled.jsx
@@ -14,26 +14,23 @@ export const FriendsStatus = styled.span`
     height: 10px;
     border-radius: 50%;
     margin-right: 10px;
-    background-color: ${({isOnline}) => {
-        console.log(isOnline);
-        switch (isOnline) {
-            case true:
-                return 'green';
-            case false:
-                return 'red';
-            default:
-                return 'white';
-        }
-    }};
-    
-
+    background-color: ${({isOnline}) => (isOnline ? 'green' : 'red')};
 `
+
 export const FriendsAvatar = styled.img`
     border-radius: 50%;
     margin-right: 10px;
 `
+
 export const FriendsName = styled.p`
     margin: 0;
     font-size: 16px;
     font-weight: bold;
+`
+
+export const FriendStatusText = styled.p`
+    margin: 0;
+    margin-left: 3px;
+    font-size: 14px;
+    color: ${({isOnline}) => (isOnline ? 'green' : 'red')};
 `
